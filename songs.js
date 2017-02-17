96e126c8663252731e4106ff49c0cb2c76012d03
sharon smith <sharon.e.smith2@gmail.com>
2017-02-16 19:02:31 -0600

    successfully writing HTML to index.html file; need to clean up output

--- songs.js
+++ songs.js
@@ -1,6 +1,5 @@
 var songs = [];
 
-<<<<<<< HEAD
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator"; // songs[0]
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America"; // songs[1]
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall"; // songs[2]
@@ -43,6 +42,7 @@ function removeChar (z, array) {
 							    // assumes the character occurs only once in the string
 
 //QUESTION :: IF CHANGING CONTENTS OF AN ARRAY, DO YOU NEED TO <RETURN> ? 
+//how does <map> function fit in with this? 
 			array[i] = array[i].substring(0,charIndex) + array[i].substring(charIndex+1);
 		}
 	}
@@ -52,7 +52,7 @@ removeChar("*", songs);
 removeChar("@", songs);
 removeChar("(", songs);
 removeChar("!", songs);
-console.log(songs);
+// console.log(songs);
 
 
 //***************************************************
@@ -62,36 +62,46 @@ console.log(songs);
 function replaceChar (oldChar, newChar, array) {
 	for (var i=0; i<array.length; i++) {
 
-		//QUESTION :: IF CHANGING CONTENTS OF AN ARRAY, DO YOU NEED TO <RETURN> ? 
+//QUESTION :: IF CHANGING CONTENTS OF AN ARRAY, DO YOU NEED TO <RETURN> ? 
+//how does <map> function fit in with this? 
 		array[i] = array[i].replace(oldChar, newChar);
 	}
 }
 
 replaceChar (">", "-", songs);
-console.log(songs);
+// console.log(songs);
 
 
 //***************************************************
 // 4. Must add each string to the DOM in index.html in the main content area.
+// What is hard-coded goes away entirely.
 //***************************************************
 
-function writeHTML (array) {
-	var sTitle = document.getElementsByClassName["songTitle"];
-	var sCredits = document.getElementsByClassName["songCredit"];
-	// console.log("songTitle", songTitle);
-	// console.log("songCredit", songCredit);
-	console.log("array[0] :: ", array[0]);
-	sTitle.innerHTML = array[0];
+// Do more cleaning on array items
+// strip out " by" ; replace "on the album" with <pipe> character
 
+// console.log(songs);
+// removeChar(" by", "");
+// console.log(songs);
+// replaceChar("on the album", "|");
+
+function writeHTML (sArray) {
+	//loop over the array / clean it up / then write innerHTML
+	for (var i=0; i<sArray.length; i++) {
+		var TargetHTML = document.getElementsByClassName("songTitle")[i];
+		// console.log(sArray);
+		// console.log("array[i] :: ", sArray[i]);
+		TargetHTML.innerHTML = sArray[i];
+		// console.log("testTargetHTML :: ", testTargetHTML);
+	}
 	// newString = "<h1>" + songTitle.innerTitle + "</h1><p>" + songCredits + "</p";
 }
 
 writeHTML(songs);
 
-=======
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
->>>>>>> 2b6e8d39aeb85be0f399cd73a77322997cee7d0c
+
