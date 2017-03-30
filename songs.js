f726d2cf5f0b92f4ad32426f73a4417a99e83bca
sharon smith <sharon.e.smith2@gmail.com>
2017-03-30 18:27:29 -0500

    JSON files created and validated + <loadJSON> function working

--- songs.js
+++ songs.js
@@ -1,29 +1,29 @@
 var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator"; // songs[0]
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America"; // songs[1]
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall"; // songs[2]
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction"; // songs[3]
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill"; // songs[4]
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator"; // songs[0]
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America"; // songs[1]
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall"; // songs[2]
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction"; // songs[3]
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill"; // songs[4]
 
 
 //***************************************************
 // 1. Each student must add one song to the beginning and the end of the array.
 //***************************************************
 
-function addSong (song, songs, where) {
-	if (where === "beginning") {
-		songs.unshift(song);
-	} else { // where === "end"
-		songs.push(song);
-	}
-}
+// function addSong (song, songs, where) {
+// 	if (where === "beginning") {
+// 		songs.unshift(song);
+// 	} else { // where === "end"
+// 		songs.push(song);
+// 	}
+// }
 
-var newSong1 = "I Can't Help You Now - by Bonnie Raitt on the album Silver Lining";
-var newSong2 = "Georgia On My Mind - by Ray Charles on the album Genius The Ultimate Ray Charles Collection";
+// var newSong1 = "I Can't Help You Now - by Bonnie Raitt on the album Silver Lining";
+// var newSong2 = "Georgia On My Mind - by Ray Charles on the album Genius The Ultimate Ray Charles Collection";
 
-addSong(newSong1, songs, "beginning");
-addSong(newSong2, songs, "end");
+// addSong(newSong1, songs, "beginning");
+// addSong(newSong2, songs, "end");
 
 
 //***************************************************
@@ -31,38 +31,38 @@ addSong(newSong2, songs, "end");
 // ie :: remove :: * @ ( !
 //***************************************************
 
-function removeChar (z, array) {
-	for (var i=0; i<array.length; i++) {
+// function removeChar (z, array) {
+// 	for (var i=0; i<array.length; i++) {
 	
-		// var charIndex = array[i].indexOf(z);
-		var charIndex = array[i].indexOf(z);
-		var newString = "";
-		if (charIndex !== -1) { // found the character to be removed
-							    // assumes the character occurs only once in the string
-			array[i] = array[i].substring(0,charIndex) + array[i].substring(charIndex+1);
-		}
-	}
-}
+// 		// var charIndex = array[i].indexOf(z);
+// 		var charIndex = array[i].indexOf(z);
+// 		var newString = "";
+// 		if (charIndex !== -1) { // found the character to be removed
+// 							    // assumes the character occurs only once in the string
+// 			array[i] = array[i].substring(0,charIndex) + array[i].substring(charIndex+1);
+// 		}
+// 	}
+// }
 
-removeChar("*", songs);
-removeChar("@", songs);
-removeChar("(", songs);
-removeChar("!", songs);
+// removeChar("*", songs);
+// removeChar("@", songs);
+// removeChar("(", songs);
+// removeChar("!", songs);
 
 
 //***************************************************
 // 3. Students must find and replace the > character in each item with a - character.
 //***************************************************
 
-function replaceChar (z, a, array) {
-	for (var i=0; i<array.length; i++) {
+// function replaceChar (z, a, array) {
+// 	for (var i=0; i<array.length; i++) {
 		
-		// array[i] = array[i].replace(z, a);
-		array[i] = array[i].replace(z, a)
-	}
-}
+// 		// array[i] = array[i].replace(z, a);
+// 		array[i] = array[i].replace(z, a)
+// 	}
+// }
 
-replaceChar (">", "-", songs);
+// replaceChar (">", "-", songs);
 
 
 //***************************************************
@@ -93,6 +93,35 @@ function writeHTML (sArray) {
 	}
 }
 
-writeHTML(songs);
+
+// writeHTML(songs);
+
+//***************************************************
+// Set of functions to execute XHR Requests
+// to load JSON files
+//***************************************************
+function executeThisCodeAfterFileLoaded(){
+	// console.log("It worked!!!", this.responseText);
+	var data = JSON.parse(this.responseText);
+	console.log("It worked!!!", data);
+	writeToDOM(data);
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("booooooo");
+}
+
+
+function loadJSON (jsonFile) {
+
+	var myRequest = new XMLHttpRequest();
+
+	myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+	myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+	myRequest.open("GET", jsonFile);
+	myRequest.send();
+}
+
+loadJSON("songs1.JSON");
 
 
