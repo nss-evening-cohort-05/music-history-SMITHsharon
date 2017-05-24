152c0a030a686f203ca8fe59bd332a5380e7dd86
sharon smith <sharon.e.smith2@gmail.com>
2017-05-24 01:10:42 -0500

    authentication code shelled out

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,5 +1,40 @@
-app.run(function(FIREBASE_CONFIG) {
-	firebase.initializeApp(FIREBASE_CONFIG);
+let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
+  if(AuthFactory.isAuthenticated()){
+    // console.log("User is authenticated, resolve route promise");
+    resolve();
+  } else {
+    // console.log("User is not authenticated, reject route promise");
+    reject();
+  }
+});
+
+app.run(function($location, $rootScope, FIREBASE_CONFIG, AuthFactory) {
+  firebase.initializeApp(FIREBASE_CONFIG);
+
+  //watch method that fires on change of a route.  3 inputs. 
+  //event is a change event
+  //currRoute is information about your current route
+  //prevRoute is information about the route you came from
+  $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute) {
+    // checks to see if there is a current user
+    var logged = AuthFactory.isAuthenticated();
+
+    var appTo;
+
+    // to keep error from being thrown on page refresh
+    if (currRoute.originalPath) {
+      // check if the user is going to the auth page = currRoute.originalPath
+      // if user is on auth page then appTo is true
+      // if it finds something other than /auth it return a -1 and -1!==-1 so resolves to false
+      appTo = currRoute.originalPath.indexOf('/auth') !== -1;
+    }
+
+    //if not on /auth page AND not logged in redirect to /auth
+    if (!appTo && !logged) {
+      event.preventDefault();
+      $location.path('/auth');
+    }
+  });
 });
 
 
@@ -7,14 +42,19 @@ app.config(function($routeProvider) {
 
 	$routeProvider
 
-	.when('/songs/list', {
-		templateUrl: 'partials/songs-list.html',
-		controller: 'SongsListCtrl'
-	})
-	.when('/song/add', {
-		templateUrl: 'partials/song-add.html',
-		controller: 'SongAddCtrl'
-	})
+    .when('/auth', {
+      templateUrl: 'partials/auth.html',
+      controller: 'AuthCtrl'
+    })
+
+  	.when('/songs/list', {
+  		templateUrl: 'partials/songs-list.html',
+  		controller: 'SongsListCtrl'
+  	})
+  	.when('/song/add', {
+  		templateUrl: 'partials/song-add.html',
+  		controller: 'SongAddCtrl'
+  	})
     .when('/song/view/:id', {
       templateUrl: 'partials/view-song.html',
       controller: 'SongViewCtrl'
@@ -23,5 +63,11 @@ app.config(function($routeProvider) {
       templateUrl: 'partials/song-add.html',
       controller: 'SongEditCtrl'
     })
-	.otherwise('/songs/list'); //default view
+
+    .when('/logout', {
+      templateUrl: 'partials/auth.html',
+      controller: 'AuthCtrl',
+      resolve : {isAuth}
+    })
+	.otherwise('/auth'); //default view
 });
---
+++ javascripts/controllers/AuthCtrl.js
@@ -0,0 +1,54 @@
+app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthFactory, UserFactory) {
+
+	// auto logon for development
+	// $scope.auth = {
+	// 	email: "",
+	// 	password: ""
+	// };
+
+
+	if ($location.path() === '/logout') {
+		AuthFactory.logout();
+		$rootScope.user = {};
+		$location.url('/auth');
+	}
+
+
+	let logMeIn = () => {
+
+		AuthFactory.authenticate($scope.auth).then((userCreds) => {
+			return UserFactory.getUser(userCreds.uid);
+		}, (error) => {
+			console.log("error on authenticate", error);
+		}).then((user) => {
+			$rootScope.user = user;
+			$location.url('/songs-list');
+		}).catch((error) => {
+			console.log("error on getUser", error);
+		});
+	};
+
+
+	$scope.registerUser = () => {
+		// new auth
+		// add user name
+		// login
+		AuthFactory.registerWithEmail($scope.auth).then((didRegister) => {
+console.log("didRegister", didRegister);
+			$scope.auth.uid = didRegister.uid;
+			return UserFactory.addUser($scope.auth);
+		}, (error) => {
+			console.log("error on registerWithEmail", error);
+		}).then((registerComplete) => {
+			logMeIn();
+		}).catch((error) => {
+			console.log("error on addUser", error);
+		});
+	};
+
+
+	$scope.loginUser = () => {
+		logMeIn();
+	};
+
+});
